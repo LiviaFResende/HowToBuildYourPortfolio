04e97b703e3c27cd98a9bc7ba4b90b8a339a783e
livia_gitConfig <your_email_address@example.comlivianejaim@gmail.com>
2021-12-15 12:17:20 -0300

    7

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 document.querySelector(".hamburguer").addEventListener("click", dropDownMenu);
 
 function dropDownMenu() {
-    document.querySelector(".desktop").classList.toggle("mobile");
+  document.querySelector(".desktop").classList.toggle("mobile");
 }
 
 // THIS IS ABOUT THE BOX WITH INSTRUCTIONS
@@ -11,10 +11,17 @@ function dropDownMenu() {
 const nextButton = document.querySelector(".nextButton")
 const thisChange1 = document.querySelector(".thisChange1")
 const thisChange2 = document.querySelector(".thisChange2")
+const previousButton = document.querySelector(".previousButton")
 
-nextButton.addEventListener("click", function(){
+nextButton.addEventListener("click", function () {
   thisChange1.innerHTML = "";
   thisChange1.innerHTML = thisChange2.innerHTML;
 })
+
+previoustButton.addEventListener("click", function () {
+  thisChange2.innerHTML = "";
+  thisChange2.innerHTML = thisChange1.innerHTML;
+})
+
 
 // How to add the new text from JS from DataBase
