6126ff2e1211651a71bed859d2e8e50b8d9f206f
livia_gitConfig <your_email_address@example.comlivianejaim@gmail.com>
2021-12-15 11:13:16 -0300

    second commmit

--- index.js
+++ index.js
@@ -2,6 +2,20 @@
 
 document.querySelector(".hamburguer").addEventListener("click", dropDownMenu);
 
-function dropDownMenu {
+function dropDownMenu() {
     document.querySelector(".desktop").classList.toggle("mobile");
 }
+
+// THIS IS ABOUT THE BOX WITH INSTRUCTIONS
+
+const mainButton = document.querySelector(".mainButton")
+const thisChange = document.querySelector(".thisChange")
+const thisChange1 = document.querySelector(".thisChange1")
+
+mainButton.addEventListener("click", function(){
+  thisChange.innerHTML = "";
+  thisChange.innerHTML = "Here goes the NewText";
+  thisChange.style.display = "none";
+})
+
+// How to add the new text from JS from DataBase
